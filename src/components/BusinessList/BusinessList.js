0435bad4d69324ef0d17c4aca241aff05ee65789
Colin <crgastelle@gmail.com>
2020-09-19 16:30:45 -0400

    completion of part 3 of ravenous project

---
+++ src/components/BusinessList/BusinessList.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './BusinessList.css';
+import Business from '../Business/Business';
+//import { ReactComponent } from '*.svg';
+
+class BusinessList extends React.Component {
+    render() {
+        return (
+            <div className="BusinessList">
+                {
+                    this.props.businesses.map(business => {
+                        return <Business business={business} />
+                    })
+                }
+            </div>
+        );
+    }
+}
+
+export default BusinessList;
